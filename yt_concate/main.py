15a8db6040ca7e08544d24eb1ab30aeed650ede0
zongweichen <jay879944@gmail.com>
2021-10-31 09:16:41 +0800

    suscessfully get video list and set requirememt.txt settings.py

---
+++ yt_concate/main.py
@@ -0,0 +1,39 @@
+import urllib.request
+import json
+import ssl
+import os
+from yt_concate.settings import API_KEY
+
+CHANNEL_ID = "UCKSVUHI9rbbkXhvAXK-2uxA"
+API_KEY = os.getenv("API_KEY")
+
+
+def get_all_video_in_channel(channel_id):
+
+
+    base_video_url = 'https://www.youtube.com/watch?v='
+    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
+                                                                                                        channel_id)
+    video_links = []
+    url = first_url
+    while True:
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+        inp = urllib.request.urlopen(url)
+        resp = json.load(inp)
+
+        for i in resp['items']:
+            if i['id']['kind'] == "youtube#video":
+                video_links.append(base_video_url + i['id']['videoId'])
+        try:
+            next_page_token = resp['nextPageToken']
+            url = first_url + '&pageToken={}'.format(next_page_token)
+        except KeyError:
+            break
+    return video_links
+
+
+video_list = get_all_video_in_channel(CHANNEL_ID)
+print(video_list)
